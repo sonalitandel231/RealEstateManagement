95c866c4156dd0162632d82a18a60f1326498b0f
sonalitandel231 <sonalitandeltandel@gmail.com>
2024-02-26 10:35:17 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="MUSA" >
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Real estate management system</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link href="assets/css/animate-custom.css" rel="stylesheet">
+
+
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    
+    <script src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+  
+  
+  	<div id="navbar-main">
+      <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon icon-shield" style="font-size:30px; color:purple;"></span>
+          </button>
+          <a class="navbar-brand hidden-xs hidden-sm" href="#home"><span class="icon icon-shield" style="font-size:18px; color:purple;"></span></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#home" class="smoothScroll"><b><font color="white">Home</font></b></a></li>
+			<li> <a href="#about" class="smoothScroll"> <b><font color="white">About Us</font></b></a></li>			
+			<li> <a href="#contact" class="smoothScroll"> <b><font color="white">Contact</font></b></a></li>
+			<li> <a href="properties.php" class="smoothScroll"><b><font color="white">Our Properties</font></b></a></li>
+			<li> <a href="#register" class="smoothScroll"><b><font color="white"> Client Registration</b></font></a></li>
+			<li> <a href="#login" class="smoothScroll"><b><font color="white">Client Login</font></b></a></li>
+			<li> <a href="landlord_reg.php" class="smoothScroll"><b><font color="white" title="Register Here">Agent Registration</font></b></a></li>
+			<li> <a href="landlordlogin.php" class="smoothScroll"><b><font color="white">Agent Login</font></b></a></li>
+			
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+    </div>
+
+  
+  
+		<!-- ==== HEADERWRAP ==== -->
+	    <div id="headerwrap" id="home" name="home">
+			<header class="clearfix">
+	  		 		
+	  		</header>	    
+	    </div><!-- /headerwrap -->
+		
+		<!-- ==== ABOUT ==== -->
+		<br>
+		<br>
+		<div class="container" id="about" name="about">
+			<br>
+				<h1 class="centered">A LITTLE ABOUT OUR AGENCY</h1>
+				<hr>
+				
+				<div class="col-lg-12" style="text-align:center">
+					<p> The main objective of our firm  is to manage the details of Property, Property Type, Property Details, Flats, Location. It manages all the information about Property, Facility, Location, Property. The project is totally built at administrative end and thus only the administrator is guaranteed the access. The purpose of the project is to build an application program to reduce the manual work for managing the Property, Property Type, Facility, Property Details. It tracks all the details about the Property Details, Flats, Location.</p>
+				</div><!-- col-lg-6 -->
+		</div><!-- container -->
+		
+		<!-- ==== SECTION DIVIDER1 -->
+		<!-- <section class="section-divider textdivider divider1">
+			<div class="container">
+				<h1>DESIGN EXPAND BOUNDARIES</h1>
+				<hr>
+				<p>To achieve real change, we have to expand boundaries. Because the Wild West of what-could-be is unexplored but rife with opportunity.</p> -->
+			</div><!-- container -->
+		</section><!-- section -->
+
+
+		
+		<!-- ==== SECTION DIVIDER6 ==== -->
+		<section class="section-divider textdivider divider6">
+			<div class="container">
+				<h1>Kadri, Mangalore.</h1>
+				<hr>
+				<p>Address 123-123-123,</p>
+				<p>123-123-123</p>
+				<p><a class="icon icon-twitter" href="#"></a> | <a class="icon icon-facebook" href="#"></a></p>
+			</div><!-- container -->
+		</section><!-- section -->
+
+		<div class="container" id="contact" name="contact">
+			<div class="row">
+			<br>
+				<h1 class="centered">THANKS FOR VISITING US</h1>
+				<hr>
+				<br>
+				<br>
+				<div class="col-lg-4">
+					<h3>Our Contact Information</h3>
+					<p><span class="icon icon-home"></span>Address 123-123-123 , Kadri, Mangalore<br/>
+						<span class="icon icon-phone"></span> 123-123-123 <br/>
+						<span class="icon icon-mobile"></span> 123-123-123 <br/>
+						<span class="icon icon-envelop"></span> <a href="#"> Buyhome</a> <br/>
+						<span class="icon icon-twitter"></span> <a href="#">  Visit Us On Twitter</a> <br/>
+						<span class="icon icon-facebook"></span> <a href="http://www.facebook.com/mtbuyhomerealestate/"> Visit Us On facebook</a>
+					</p>
+				</div><!-- col -->
+				
+				
+				<div class="col-lg-4">
+					<h3>Reach Us Now</h3>
+					<p>Send us a message now and see your dream come true</p>
+					<p>
+						<form class="form-horizontal" role="form" action="sendmessage.php" method="POST">
+							 <div class="form-group">
+						    <label for="name" class="col-lg-4 control-label"></label>
+						    <div class="col-lg-10">
+						      <input type="text" class="form-control" id="text1" name="name" placeholder="Enter Your Name">
+						    </div>
+						 </div>
+						     <div class="form-group">
+						    <label for="email" class="col-lg-4 control-label"></label>
+						    <div class="col-lg-10">
+						      <input type="email" class="form-control" id="text1" name="email" placeholder="Enter Your Email">
+						    </div>
+						  </div>
+                              
+                               <div class="form-group">
+						    <label for="phone" class="col-lg-4 control-label"></label>
+						    <div class="col-lg-10">
+						      <input type="text" class="form-control" id="text1" name="phone" placeholder="Enter Your Phone Number">
+						    </div>
+						  </div>
+
+						  <div class="form-group">
+                                        <textarea name="message" id="message" required="required" class="form-control" rows="8" name="message" placeholder="Your Message"></textarea>
+                                    </div>
+						
+						 
+						  <div class="form-group">
+						    <div class="col-lg-10">
+						      <button type="submit" name="submit" class="btn btn-success">SEND</button>
+						    </div>
+						  </div>
+
+					   </form><!-- form -->
+					</p>
+				</div><!-- col -->
+				
+				<div class="col-lg-4">
+					<h3>We Are Always At Your Service</h3>
+					<p>At Mt. buyhome Real Estate Developers Ltd. , We are committed to serve you better.</p>
+				</div><!-- col -->
+
+			</div><!-- row -->
+
+
+		
+		</div><!-- container -->
+
+		
+		<div class="container" id="register" name="register">
+			<div class="row">
+			<br>
+				<h1 class="centered">REGISTER NOW</h1>
+				<hr>
+				<br>
+				<br>
+				
+				<div class="">
+					<center><h3>Register With Us Now</h3>
+					<p>We value our clients and we offer the best services regarding real estate properties</p></center>
+					<p>
+
+                       <center>
+						<form class="" role="form" action="register.php" method="POST">
+							<div class="col-lg-10">
+						      <input type="text" class="form-control" id="text1" placeholder="Enter Your Full Name" required="required" name="fname">
+						    </div>
+						    <br>
+						    <br>
+						    <div class="col-lg-10">
+						      <input type="tel" class="form-control" id="text1" placeholder="Enter Your Phone Number" required="required" name="phone" pattern="[7-9]{1}[0-9]{9}">
+						    </div>
+						    <br>
+						    <br>
+						    <div class="col-lg-10">
+						      <input type="email" class="form-control" id="text1" placeholder="Enter Your Email Address" required="required" name="email">
+						    </div>
+						    <br>
+						    <br>
+						     <div class="col-lg-10">
+						    <tr>
+                            <td>Select Your Gender:</td>
+                            <td>
+                                <select name="gender" required="required">
+                                    <option> Select Gender </option>
+                                    <option> Male </option>
+                                    <option> Female </option>
+                                </select>
+                            </td>
+                        </tr>
+                    </div>
+                            <br>
+						    <br>
+						     <div class="col-lg-10">
+						      <input type="text" class="form-control" id="text1" placeholder="Enter Your Location" required="required" name="location">
+						    </div>
+						    <br>
+						    <br>
+						     <div class="col-lg-10">
+						      <input type="password" class="form-control" id="text1" placeholder="Enter Password You Will Use For Login" required="required" name="password" pattern="(?=.\d)(?=.[a-z])(?=.*[A-Z]).{8,}">
+						    </div>
+                            <br>
+						    <br>
+                        </tr>
+						 
+						  <div class="form-group">
+						    <div class="col-lg-10">
+						      <button type="submit" name="submit" class="btn btn-success">REGISTER</button>
+						    </div>
+						  </div>
+					   </form><!-- form -->
+						
+					</p>
+				</div><!-- col -->
+		
+			</div><!-- row -->
+		
+		</div><!-- container -->
+
+		<!-- Login Block -->
+<div class="container" id="login" name="login">
+			<div class="row">
+			<br>
+				<h1 class="centered">LOGIN NOW</h1>
+				<hr>
+				<br>
+				<br>
+				
+				<div class="">
+					<center><h3><font size="10px;"><b>Client Login</b></font></h3>
+					<p>Login and Book Your Preferred Property Now</p></center>
+					<p>
+						<form class="" role="form" action="account.php" method="POST">
+						  
+						  <div class="col-lg-10">
+						      <input type="text" class="form-control" id="text1" placeholder="Enter Username" name="username">
+						    </div>
+						    <br>
+						    <br>
+						     <div class="col-lg-10">
+						      <input type="password" class="form-control" id="text1" placeholder="Enter Password" name="password">
+						    </div>
+						    <br>
+						    <br>
+						  <div class="form-group">
+						    <div class="col-lg-10">
+						      <center><button type="submit" name="log" class="btn btn-success">LOGIN</button></center>
+						    </div>
+						    <center><a href="forgot_pass.php"><u>Forgot Password?</u></a></center>
+						  </div>
+					   </form><!-- form -->
+					</p>
+				</div><!-- col -->
+				
+				<div class="col-lg-4">
+					<h3>Thank You</h3>
+					<p>Thankyou for having an interest in Mt.buyhome Real Estate and Developers Ltd. Always keep in touch.</p>
+				</div><!-- col -->
+
+			</div><!-- row -->
+		
+		</div><!-- container -->
+
+		<div id="footerwrap">
+			<div class="container">
+				<h4>Created With Love by <a href="https://www.facebook.com">Ramya And Shraddha</a> - Copyright 2022</h4>
+			</div>
+			<li> <a href="adminlogin.php" class="smoothScroll"><font color="red"><b><font color="white">Administrator</font></b></font></a></li>
+		</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+		
+    <script type="text/javascript" src="assets/js/jquery-func.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/retina.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+	
+  </body>
+</html>
---
+++ landlord/menu.php
@@ -0,0 +1,29 @@
+
+<?php
+	//session_start();
+	// if(!$_SESSION['uname'] && (!$_SESSION['pass'])){
+	// 	//header("location: ../login.php");
+	// }
+	// else{
+	// $user=$_SESSION['uname'];
+
+	// }
+?>
+<div id="top">
+			<h1><a href="#">Real Estate Management System</a></h1>
+			<div id="top-navigation">
+				<!-- Welcome <a href="#"><strong></strong></a>										 -->
+				<span>|</span>
+				<a href="logout.php">Log out</a>
+			</div>
+		</div>
+<div id="navigation">
+			<ul>
+			    <li><a href="index.php"><span>Dashboard</span></a></li>
+			    <li><a href="my_houses.php"><span>View Houses</span></a></li>
+			    <li><a href="add_house.php"><span>Add House</span></a></li>
+			    
+				
+			    
+			</ul>
+		</div>
